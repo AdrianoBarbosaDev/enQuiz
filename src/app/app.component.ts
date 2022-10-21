17281e4018bf706573b5423557f072300a0999ad
adriano.bsilva <adriano.bsilva@qintess.com>
2022-10-21 16:45:27 -0300

    Feio mas funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { BoasVindasComponent } from './boas-vindas/boas-vindas.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private dialogRef : MatDialog) {
+
+ 
+
+  }
+
+  ngOnInit(): void {
+    this.openDialog();
+  }
+  openDialog(){
+    this.dialogRef.open(BoasVindasComponent)
+  }
+
   title = 'app1';
 
   public jogoEmAndamento:Boolean = true
