a9120ec0a4300b490ae1a83cc0868a6563df45a5
Adriano Barbosa da Silva <adrianobarbosa1999@gmail.com>
2023-03-07 19:42:35 -0300

    Altera frases para palavras e inclui modal da issue #6

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,8 +1,8 @@
 import { Frase } from "../shared/frase.model";
 
 export const Frases: Array<Frase> = [
-    {fraseEng:"How are you",frasePtbr: "Como você está"},
-    {fraseEng:"I like to learn",frasePtbr:"Eu gosto de Aprender"},
-    {fraseEng:"I watch TV",frasePtbr:"Eu assisto televisão"},
-    {fraseEng:"I eat bread",frasePtbr:"Eu como pão"}
+    {fraseEng:"Circle",frasePtbr: "Circulo"},
+    {fraseEng:"Learn",frasePtbr:"Aprender"},
+    {fraseEng:"Keyboard",frasePtbr:"Teclado"},
+    {fraseEng:"Table",frasePtbr:"Mesa"}
 ]
